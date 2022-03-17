10616830849394e30918cc9c1032faecfc3a690c
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-16 17:32:08 -0700

    Added some css elements to try to make it look a little more exciting

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ const input2 = document.getElementById('input-option2');
 const current1 = document.getElementById('holder-1');
 const add1 = document.getElementById('add-1');
 const sub1 = document.getElementById('subtract-1');
-//const counter1 = document.getElementById('current-count-1');
 const current2 = document.getElementById('holder-2');
 const add2 = document.getElementById('add-2');
 const sub2 = document.getElementById('subtract-2');
-//const counter2 = document.getElementById('current-count-2');
 const finis = document.getElementById('finish-button');
 const pastPoll = document.getElementById('ready-for-past-polls');
 const currentPoll = document.getElementById('current-polling');
-console.log(input1, addButton, input2, current1, add1, sub1, current2, add2, sub2, finis, pastPoll, currentPoll);
-  
+const questionInput = document.getElementById('question-input');
+const currentQ = document.getElementById('question');
+
 
 // let state
 let count1 = 0;
 let count2 = 0;
+let question = '';
 let poll1 = '';
 let poll2 = '';
 let past = [];
@@ -30,9 +30,11 @@ let past = [];
   // use user input to update state 
   // update DOM to reflect the new state
 addButton.addEventListener('click', () => {
+    question = questionInput.value;
     poll1 = input1.value;
     poll2 = input2.value;
 
+    questionInput.value = '';
     input1.value = '';
     input2.value = '';
 
@@ -61,6 +63,7 @@ sub2.addEventListener('click', () => {
 
 finis.addEventListener('click', () => {
     const pastPolls = {
+        question: question,
         poll1: poll1,
         poll2: poll2,
         count1: count1,
@@ -68,6 +71,7 @@ finis.addEventListener('click', () => {
     };
     past.push(pastPolls);
     displayPolls();
+    question = '';
     poll1 = '';
     poll2 = '';
     count1 = 0;
@@ -77,16 +81,18 @@ finis.addEventListener('click', () => {
 
 function refreshCurrentPoll() {
     currentPoll.textContent = '';
+    currentQ.textContent = question;
     current1.textContent = poll1;
     current2.textContent = poll2;
-    const pollEl = renderPoll(poll1, poll2, count1, count2);
+    const pollEl = renderPoll(question, poll1, poll2, count1, count2);
     pollEl.classList.add('current');
     currentPoll.append(pollEl);
 }
 function displayPolls() {
     pastPoll.textContent = '';
     for (let pastPollObj of past) {
-        const pastPollEl = renderPoll(pastPollObj.poll1, pastPollObj.poll2, pastPollObj.count1, pastPollObj.count2);
+        const pastPollEl = renderPoll(pastPollObj.question, pastPollObj.poll1, pastPollObj.poll2, pastPollObj.count1, pastPollObj.count2);
+        pastPoll.classList.add('past-poll');
         pastPoll.append(pastPollEl);
     }
 }
